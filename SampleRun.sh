78fc5bb276b2936957e5d8045b838dcd224b3f5e
L-Joshua-Y <joshuayao13@gmail.com>
2023-03-15 22:47:38 +0800

    Modify the shell script

--- SampleRun.sh
+++ SampleRun.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Sample Enclave"
 /opt/alibaba/SGXProgram/SampleCode/SampleEnclave/app
+
+echo "Generate Quote"
 /opt/alibaba/SGXProgram/SampleCode/QuoteGenerationSample/app
+
+echo "Verify Quote"
 /opt/alibaba/SGXProgram/SampleCode/QuoteVerificationSample/app
