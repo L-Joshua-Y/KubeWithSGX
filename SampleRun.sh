96ee7fd65aa6873d4d32386401b25e6d4839b459
L-Joshua-Y <joshuayao13@gmail.com>
2023-03-15 21:51:30 +0800

    Add a shell script

---
+++ SampleRun.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/opt/alibaba/SGXProgram/SampleCode/SampleEnclave/app
+/opt/alibaba/SGXProgram/SampleCode/QuoteGenerationSample/app
+/opt/alibaba/SGXProgram/SampleCode/QuoteVerificationSample/app
