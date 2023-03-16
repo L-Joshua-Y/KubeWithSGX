3165f9721742481cf347bb70095be2fd9c2e0f79
L-Joshua-Y <joshuayao13@gmail.com>
2023-03-16 22:20:11 +0800

    Update shell script

--- SampleRun.sh
+++ SampleRun.sh
@@ -4,8 +4,16 @@ cd /opt/alibaba/SGXProgram/SampleCode/SampleEnclave && ./app
 
 echo ""
 echo "Generate Quote"
-cd /opt/alibaba/SGXProgram/SampleCode/QuoteGenerationSample && ./app && ls -ll
+cd /opt/alibaba/SGXProgram/SampleCode/QuoteGenerationSample && ./app
+
+echo ""
+echo "QuoteGenerationSample Directory"
+ls -ll /opt/alibaba/SGXProgram/SampleCode/QuoteGenerationSample
 
 echo ""
 echo "Verify Quote"
-cd /opt/alibaba/SGXProgram/SampleCode/QuoteVerificationSample && ./app && ls -ll ../QuoteGenerationSample
+cd /opt/alibaba/SGXProgram/SampleCode/QuoteVerificationSample && ./app
+
+echo ""
+echo "QuoteGenerationSample Directory"
+cd /opt/alibaba/SGXProgram/SampleCode/QuoteVerificationSample && ls -ll ../QuoteGenerationSample
